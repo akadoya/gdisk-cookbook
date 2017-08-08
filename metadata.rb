6b33a7b86998a537cf4af90f7fb00d1498323e9a
akadoya <aoi.kadoya@rakops.com>
2017-08-08 11:29:30 -0700

    updating metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'cadyan.aoi@gmail.com'
 license 'All Rights Reserved'
 description 'Provides gdisk resource'
 long_description 'Provides gdisk resource to partition disks'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-issues_url 'https://github.com/akadoya/gdisk/issues'
-source_url 'https://github.com/akadoya/gdisk'
+issues_url 'https://github.com/akadoya/gdisk-cookbook/issues'
+source_url 'https://github.com/akadoya/gdisk-cookbook'
 
 supports 'ubuntu', '= 16.04'
