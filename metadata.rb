1c8da0c6cdca1ab1bcf20a06b688e530a76ec91d
akadoya <aoi.kadoya@rakops.com>
2017-08-09 18:12:33 -0700

    corrected license

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'gdisk'
 maintainer 'Aoi Kadoya'
 maintainer_email 'cadyan.aoi@gmail.com'
-license 'All Rights Reserved'
+license 'Apache v2.0'
 description 'Provides gdisk resource'
 long_description 'Provides gdisk resource to partition disks'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/akadoya/gdisk-cookbook/issues'
